1a20581978e8a557adab0173aea832e3ddeda107
Martin Ritter <maritter@umd.edu>
2022-07-21 14:22:07 -0400

    Fixed the last theory code, have not test yet since Flint install is a pain

--- theory_codes/PauliCode_GenGraph.py
+++ theory_codes/PauliCode_GenGraph.py
@@ -36,32 +36,26 @@ from flint import nmod_mat
 #solve
 #transpose
 #table
-
-
-KollarLabClassPath = r'/Users/kollar2/Documents/KollarLab/MainClasses/GraphCodes'
-if not KollarLabClassPath in sys.path:
-    sys.path.append(KollarLabClassPath)
-
-    
-from CDSconfig import CDSconfig
+   
+from .CDSconfig import CDSconfig
 
 
    
-from GeneralLayoutGenerator import GeneralLayout
-from GeneralLayoutGenerator import TreeResonators
+from GraphCodes.GeneralLayoutGenerator import GeneralLayout
+from GraphCodes.TreeResonators import TreeResonators
 
-from EuclideanLayoutGenerator2 import UnitCell
-from EuclideanLayoutGenerator2 import EuclideanLayout
+from GraphCodes.UnitCell import UnitCell
+from GraphCodes.EuclideanLayoutGenerator2 import EuclideanLayout
 
-from LayoutGenerator5 import PlanarLayout
+from GraphCodes.LayoutGenerator5 import PlanarLayout
 
 
-from GeneralLayoutGenerator import split_resonators
-from GeneralLayoutGenerator import rotate_resonators
-from GeneralLayoutGenerator import generate_line_graph
-from GeneralLayoutGenerator import shift_resonators
+from GraphCodes.resonator_utility import split_resonators
+from GraphCodes.resonator_utility import rotate_resonators
+from GraphCodes.resonator_utility import generate_line_graph
+from GraphCodes.resonator_utility import shift_resonators
 
-from PauliCode import PauliCode
+from .PauliCode import PauliCode
 
 
 
